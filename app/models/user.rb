f06a07a885d7e9eb19ba463032894ab6e05bdee2
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-06-29 12:29:29 -0400

    Add validation for User.email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, length: { in: 3..20 }, uniqueness: true, presence: true 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, presence: true
 
   has_many :posts
   has_many :comments
